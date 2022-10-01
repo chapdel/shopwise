f926216450155c0de25a2224b3e2cbf078220885
Chapdel KAMGA <drop@chapdel.me>
2022-10-01 22:37:07 +0100

    Fixed user regislation

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'last_name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
